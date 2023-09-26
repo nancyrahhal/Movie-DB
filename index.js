3da76d1af46cd725306d881b79059bcdc6e8d78d
Nancy Rahhal <nancyrahhal97@gmail.com>
2023-09-26 22:12:52 +0300

    step 3

--- index.js
+++ index.js
@@ -3,4 +3,14 @@ let app = express();//creating an instance of Express
 let port = 3000;//specifying the port
 
 app.get('/',(req, res)=> res.send('ok'));//implementin the response behaviour
+app.get('/test',(req,res)=>{
+    res.status(200).json({status:200, message:'ok'});
+});
+app.get('/time',(req,res)=>{
+    let currentTime= new Date();
+    let hour=currentTime.getHours();
+    let minute=currentTime.getMinutes()
+    res.status(200).json({status:200, message: `${hour}:${minute<10?'0':''}${minute}`});
+});
 app.listen(port,()=>console.log('Express app is running on port 3000'))
+
